a230d920ee875ffee77937f28ec6157cabc850fe
sashaitk <sasha.itkis@chainguard.dev>
2026-04-17 14:45:52 -0400

    initial commit

--- models/customers.sql
+++ models/customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from sasha-chainguard.dbt_tutorial.raw_customers
 
 ),
 
@@ -17,7 +17,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from sasha-chainguard.dbt_tutorial.raw_orders
 
 ),
 
